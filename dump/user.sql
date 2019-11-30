a3d54c27d279f291e38fa816f7c3027327763e55
Krzysztof Madeński <s15540@pjwstk.edu.pl>
2019-11-30 09:47:06 +0100

    Hasło do przykładowych userów

--- dump/user.sql
+++ dump/user.sql
@@ -1,6 +1,6 @@
 INSERT INTO role (id,name) VALUES (1,'ROLE_USER');
 INSERT INTO role (id,name) VALUES (2,'ROLE_ADMIN');
-
+-- hasło w obu przypadkach to "123"
 INSERT INTO person (id,email, password, name) VALUES(1,'user@mas.pl','$argon2i$v=19$m=65536,t=4,p=1$d2NHWkNTRVdYS2k5T0hIQw$WmDx/yBdlUn9O0y8Bzq2yVavwxDidZ+8eJ2GeuI/3Qc','example_user');
 INSERT INTO person (id,email, password, name) VALUES(2,'admin@mas.pl','$argon2i$v=19$m=65536,t=4,p=1$d2NHWkNTRVdYS2k5T0hIQw$WmDx/yBdlUn9O0y8Bzq2yVavwxDidZ+8eJ2GeuI/3Qc','example_admin');
 
